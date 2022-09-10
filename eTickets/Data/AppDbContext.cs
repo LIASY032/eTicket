056b5074538a868ee8f034bce48dae6ebad6be61
LIASY032 <liasy032@mymail.unisa.edu.au>
2022-09-10 16:53:05 +0930

    ShoppingCartItem added

--- eTickets/Data/AppDbContext.cs
+++ eTickets/Data/AppDbContext.cs
@@ -36,5 +36,8 @@ namespace eTickets.Data
         //Orders related tables
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderItem> OrderItems { get; set; }
+
+
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
     }
 }
