fb0a761bb2e415c9290db71946210dc32c45f52a
LIASY032 <liasy032@mymail.unisa.edu.au>
2022-09-10 16:58:39 +0930

    OrdersController added 2

--- eTickets/Data/ViewModels/ShoppingCartVM.cs
+++ eTickets/Data/ViewModels/ShoppingCartVM.cs
@@ -1,6 +1,14 @@
-﻿namespace eTickets.Data.ViewModels
+﻿using eTickets.Data.Cart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.ViewModels
 {
     public class ShoppingCartVM
     {
+        public ShoppingCart ShoppingCart { get; set; }
+        public double ShoppingCartTotal { get; set; }
     }
 }
