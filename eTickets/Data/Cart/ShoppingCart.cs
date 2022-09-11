27508388fd2ad72711843ca4259e6b30301ae11c
LIASY032 <liasy032@mymail.unisa.edu.au>
2022-09-11 15:11:52 +0930

    Order sections and User Profile added

--- eTickets/Data/Cart/ShoppingCart.cs
+++ eTickets/Data/Cart/ShoppingCart.cs
@@ -74,12 +74,19 @@ namespace eTickets.Data.Cart
             }
             _context.SaveChanges();
         }
-
+        public async Task ClearShoppingCartAsync()
+        {
+            var items = await _context.ShoppingCartItems.Where(n => n.ShoppingCartId == ShoppingCartId).ToListAsync();
+            _context.ShoppingCartItems.RemoveRange(items);
+            await _context.SaveChangesAsync();
+        }
         public List<ShoppingCartItem> GetShoppingCartItems()
         {
             return ShoppingCartItems ?? (ShoppingCartItems = _context.ShoppingCartItems.Where(n => n.ShoppingCartId == ShoppingCartId).Include(n => n.Movie).ToList());
         }
 
         public double GetShoppingCartTotal() => _context.ShoppingCartItems.Where(n => n.ShoppingCartId == ShoppingCartId).Select(n => n.Movie.Price * n.Amount).Sum();
+
+
     }
 }
